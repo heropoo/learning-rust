a2a4a6139236e36a6e52d39ea03ae566e9474f52
heropoo <aiyouyou1000@163.com>
2019-07-10 16:40:39 +0800

    plastic

---
+++ codes/function/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    //another_function(-5);
+    //let x = 5;
+    //let y = {
+    //    let x = 3;
+    //    x + 1
+    //};
+
+    //println!("The value of y is: {}", y);
+    let x = five();
+    let x = plus_one(x);
+
+    println!("The value of x is: {}", x);
+}
+
+//fn another_function(x:i32){
+//    println!("The value of x is: {}", x);
+//}
+fn five() -> i32{
+    5
+}
+fn plus_one(x: i32) -> i32{
+    x + 1
+}
---
+++ codes/variables/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    //let mut x = 5;
+    //println!("The value of x is: {}", x);
+
+    //x = 6;
+    //println!("The value of x is: {}", x);
+
+    //const MAX_POINTS: u32 = 100_100;
+    //println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+
+    //let x = 5;
+    //let x = x + 1;
+    //let x = x * 2;
+    //println!("The value of x is: {}", x);
+
+    //let spaces = "    ";
+    //let spaces = spaces.len();
+    //println!("The value of spaces is: {}", spaces);
+    
+    // type error
+    //let spaces = "    ";
+    //spaces = spaces.len();
+    //println!("The value of spaces is: {}", spaces);
+    
+    //let guess = "42".parse().expect("Not a number!");
+    //let guess:u32 = "42".parse().expect("Not a number!");
+
+    //let tup: (i32, f64, u8) = (500, 6.4, 1);
+    //let tup = (500, 6.4, 1);
+    //let (x, y, z) = tup;
+
+    //println!("The value of y is: {}", y);
+    //let x: (i32, f64, u8) = (500, 6.4, 1);
+    //let five_hundred = x.0;
+    //let six_point_four = x.1;
+    //let one = x.2;
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("The value of a[2] is :{}", a[2]);
+}
