e9f29e65743672489ce9fdf982df991519026c53
heropoo <aiyouyou1000@163.com>
2019-07-11 13:39:57 +0800

    test

---
+++ codes/ownership/test_4.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+//引用
+fn calculate_length(s: &String) -> usize {  // s 是对 String 的引用
+    s.len()
+} // 这里，s 离开了作用域。但因为它并不拥有引用值的所有权，
+  // 所以什么也不会发生
