3c19ed67aa7daa6e2d76fde2631f5359fcfa453c
heropoo <aiyouyou1000@163.com>
2019-07-15 16:48:52 +0800

    strcuture

--- codes/structure/user.rs
+++ codes/structure/user.rs
@@ -1,15 +1,28 @@
-fn main(){
-    struct User{
-        username: String,
-        email: String,
-        sign_in_count: u64,
-        active: bool
-    }
 
-    let user1 = User{
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let mut user1 = User {
         email: String::from("test@test.com"),
         username: String::from("test"),
         active: true,
-        sign_in_count: 1
+        sign_in_count: 1,
     };
+
+    user1.email = String::from("a@test.com");
+    let user2 = build_user(String::from("test2@test.com"), String::from("test2"));
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
