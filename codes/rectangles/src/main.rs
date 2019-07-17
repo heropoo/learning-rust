b934a263866cc21e4ce78933b5df86849af32f16
heropoo <aiyouyou1000@163.com>
2019-07-17 21:45:28 +0800

    practice

--- codes/rectangles/src/main.rs
+++ codes/rectangles/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool{
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
 }
 
 fn main() {
@@ -33,6 +37,10 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    
+    println!("The value of sq is {:?}", sq);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
