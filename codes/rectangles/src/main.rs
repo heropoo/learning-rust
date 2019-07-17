3b7e12ccef526eb78cdb0865a7fcc7e3da35a326
heropoo <aiyouyou1000@163.com>
2019-07-17 19:24:38 +0800

    practice

--- codes/rectangles/src/main.rs
+++ codes/rectangles/src/main.rs
@@ -4,6 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -14,8 +24,15 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        //area(&rect1)
+        rect1.area()
     );
+
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
