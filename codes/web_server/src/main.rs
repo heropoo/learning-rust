90ca83414168acd2a7edffd30317e92c3ad6d659
heropoo <aiyouyou1000@163.com>
2019-07-18 19:02:21 +0800

    httparse

--- codes/web_server/src/main.rs
+++ codes/web_server/src/main.rs
@@ -3,6 +3,8 @@ use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
 
+extern crate httparse;
+
 fn main() {
     let host = "127.0.0.1:7878";
     let listener = TcpListener::bind(host).unwrap();
@@ -20,9 +22,27 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    
-    //println!("Reqeust: {}", String::from_utf8_lossy(&buffer[..]));
-    
+
+    println!("Reqeust: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+
+//    println!("Reqeust Method: {}", get_request_method(&request));
+//    println!("Reqeust Method: {}", Method::from_bytes(&request));
+//    println!("Reqeust URL: {}", get_request_uri(&request));
+//    println!("Reqeust URL: {}", get_request_uri(&request));
+
+
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    let res = req.parse(request.as_bytes()).unwrap();
+    if res.is_complete() {
+        println!("Request URI: {:?}", req.path);
+        println!("Request Method: {:?}", req.method);
+        println!("Request Version: {:?}", req.version);
+        println!("Request Headers: {:?}", req.headers);
+    }
+
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
@@ -36,3 +56,36 @@ fn handle_connection(mut stream: TcpStream){
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
+
+//fn get_request_method(s: &str) -> &str{
+//    let bytes = s.as_bytes();
+//    for (i, &item) in bytes.iter().enumerate(){
+//        if item == b' ' {
+//            return &s[0..i];
+//        }
+//    }
+//    return "";
+//}
+//
+//fn get_request_uri(s: &str) -> &str{
+//
+//    let mut start = 0;
+//    let mut end = 0;
+//    let mut count = 0;
+//
+//    let bytes = s.as_bytes();
+//    for (i, &item) in bytes.iter().enumerate(){
+//        if item == b' ' {
+//            //return &s[0..i];
+//            if count > 0 {
+//                end = i;
+//                break;
+//            }else{
+//                start = i;
+//                count += 1;
+//            }
+//        }
+//    }
+//
+//    return &s[start..end];
+//}
