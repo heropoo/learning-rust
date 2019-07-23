886261608a0851d85cd6be8e496362b3cf0da62c
heropoo <aiyouyou1000@163.com>
2019-07-23 11:54:26 +0800

    practice

---
+++ codes/my-project/src/bin/main.rs
@@ -0,0 +1,11 @@
+use my_project::plant;
+
+fn main() {
+    let mut v = plant::Vegetable::new("squash");
+
+    v.name = String::from("butternut squash");
+    println!("{} are delicious", v.name);
+
+    // 如果将如下行取消注释代码将无法编译:
+    //println!("The ID is {}", v.id);
+}
---
+++ codes/my-project/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod plant {
+    pub struct Vegetable {
+        pub name: String,
+        id: i32,
+    }
+
+    impl Vegetable {
+        pub fn new(name: &str) -> Vegetable {
+            Vegetable {
+                name: String::from(name),
+                id: 1,
+            }
+        }
+    }
+}
