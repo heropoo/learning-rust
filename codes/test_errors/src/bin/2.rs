b73a922d7ee319238307a2633c5b7f1207c1abf7
heropoo <aiyouyou1000@163.com>
2019-08-01 11:41:44 +0800

    errors

---
+++ codes/test_errors/src/bin/2.rs
@@ -0,0 +1,4 @@
+use std::fs::File;
+fn main(){
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
---
+++ codes/test_errors/src/bin/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    //println!("Hello, world!");
+    //panic!("crash and return");
+    let v = vec![1, 2,3];
+    v[99];
+}
