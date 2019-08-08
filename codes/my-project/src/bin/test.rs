8c47e2d43125d631aef2f8eb802a7aa176fd3855
heropoo <aiyouyou1000@163.com>
2019-08-08 11:18:25 +0800

    trait

---
+++ codes/my-project/src/bin/test.rs
@@ -0,0 +1,13 @@
+use my_project::Summary;
+use my_project::Tweet;
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
