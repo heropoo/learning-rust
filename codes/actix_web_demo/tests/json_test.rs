4304ad830c705332083d7a3e0ae14489b806df2a
heropoo <aiyouyou1000@163.com>
2019-08-14 10:46:29 +0800

    test json

--- codes/actix_web_demo/tests/json_test.rs
+++ codes/actix_web_demo/tests/json_test.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-//use serde_json::Result;
+use serde_json::{Result, Value};
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -8,6 +8,7 @@ struct User {
     password: String,
     created_at: u32,
     updated_at: u32,
+    phones: Vec<String>
 }
 
 #[test]
@@ -18,12 +19,50 @@ fn json_encode() {
         password: String::from("123456"),
         created_at: 12312,
         updated_at: 1232134343,
+        phones: vec!["13812341234".to_string(), String::from("13812341235")]
     };
 
     // Serialize it to a JSON string.
     let user_string = serde_json::to_string(&user).unwrap();
 
     println!("{}", user_string);
+}
 
+#[test]
+fn json_decode(){
+    let user_string = r#"
+    {
+        "id":1,
+        "username":"小明",
+        "password":"123456",
+        "created_at":12312,
+        "updated_at":1232134343
+    }"#;
+
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(user_string).unwrap();
+
+    // Access parts of the data by indexing with square brackets.
+    println!("Please call {} at the number {}", v["username"], v["created_at"]);
+    assert_eq!(1, 1)
+}
+
+#[test]
+fn json_decode2(){
+    let user_string = r#"
+    {
+        "id":1,
+        "username":"小明",
+        "password":"123456",
+        "created_at":12312,
+        "updated_at":1232134343,
+        "phones":["13812341234","13812341235"]
+    }"#;
+
+    // Parse the string of data into serde_json::Value.
+    let user: User = serde_json::from_str(user_string).unwrap();
+
+    // Access parts of the data by indexing with square brackets.
+    println!("Please call {} at the number {}", user.username, user.phones[0]);
     assert_eq!(1, 1)
 }
