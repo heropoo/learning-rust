aa57c8ad4011170d19149d6efb284cd0541067a6
heropoo <aiyouyou1000@163.com>
2019-08-14 11:05:23 +0800

    test time

--- codes/actix_web_demo/tests/json_test.rs
+++ codes/actix_web_demo/tests/json_test.rs
@@ -1,13 +1,15 @@
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
+use std::time::{SystemTime, UNIX_EPOCH};
+
 
 #[derive(Serialize, Deserialize)]
 struct User {
     id: u32,
     username: String,
     password: String,
-    created_at: u32,
-    updated_at: u32,
+    created_at: u64,
+    updated_at: u64,
     phones: Vec<String>
 }
 
@@ -17,8 +19,8 @@ fn json_encode() {
         id: 1,
         username: String::from("小明"),
         password: String::from("123456"),
-        created_at: 12312,
-        updated_at: 1232134343,
+        created_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+        updated_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
         phones: vec!["13812341234".to_string(), String::from("13812341235")]
     };
 
